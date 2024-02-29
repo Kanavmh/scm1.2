9e404637eaade79de7ca4e1f8d4a8e37f3273ba3
Kanav Mahajan <kanavmahj@gmail.com>
2024-02-29 11:18:08 +0530

    Create 29feb.py

---
+++ 29feb.py
@@ -0,0 +1,4 @@
+def print_group_description():
+    print("A group is a collection of individuals who come together for a common purpose or shared interest. It provides a platform for collaboration, communication, and coordination among its members. Groups can be found in various domains, such as social clubs, professional organizations, and online communities. They foster a sense of belonging, facilitate knowledge sharing, and promote teamwork.")
+    
+print_group_description()
